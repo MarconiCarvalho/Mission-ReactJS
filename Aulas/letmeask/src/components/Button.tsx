874691344e99a0e3d4d35cab22cc83ea18c2abac
Marconi de Carvalho Alves Junior <marconi.carvalho.bcc@gmail.com>
2021-06-21 20:44:29 -0300

    configurando ambiente

--- Aulas/letmeask/src/components/Button.tsx
+++ Aulas/letmeask/src/components/Button.tsx
@@ -1,10 +1,16 @@
-type ButtonProps = {
-    
-text?: number;
+import{useState} from "react";
 
-}
-export function Button(props: ButtonProps){
+export function Button(){
+   // let counter = 0;
+    const [counter, setCounter] = useState(0)
+   
+    function increment(){
+        setCounter(counter + 1);
+        console.log(counter);
+    }
     return(
-        <button>{props.text || 'Default' }</button>
+        <button onClick={increment}>
+            {counter}
+        </button>
     )
 }
