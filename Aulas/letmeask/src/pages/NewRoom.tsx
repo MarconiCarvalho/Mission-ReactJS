7ba6f1a5c277ae5b315dcb38a083aeb9021090f5
Marconi de Carvalho Alves Junior <marconi.carvalho.bcc@gmail.com>
2021-06-23 09:07:32 -0300

    Navegação da aplicação

--- Aulas/letmeask/src/pages/NewRoom.tsx
+++ Aulas/letmeask/src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+    import { Link } from 'react-router-dom'
     import illustrationImg from '../assets/images/illustration.svg';
     import logoImg from '../assets/images/logo.svg';
     import googleIconImg from '../assets/images/google-icon.svg';
@@ -9,7 +10,7 @@
             <div id ="page-auth">
                 <aside>
                     <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
-                    <strong>Crie Salas de Q&amo; A ao-vivo</strong>
+                    <strong>Crie Salas de Q &amp; A ao-vivo</strong>
                     <p>Tire as dúvidas de sua audiência em tempo-real</p>
                 </aside>
                 <main>
@@ -27,7 +28,7 @@
                             </Button>
                         </form>
                         <p>
-                            Quer entrar em uma sala existente?<a href= "#">Clique aqui</a>
+                            Quer entrar em uma sala existente?<Link to= "/">Clique aqui</Link>
                         </p>
                     </div>
                 </main>
